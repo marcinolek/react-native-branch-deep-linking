ecf8920672b9ce0eed436ae4901763c6eb00b557
Marcin Olek <marcin@spotio.com>
2018-10-25 17:23:15 +0200

    our rn branch

--- examples/webview_example_native_ios/WebViewExample/WebViewExample-BridgingHeader.h
+++
@@ -1,9 +0,0 @@
-//
-//  WebViewExample-BridgingHeader.h
-//  WebViewExample
-//
-//  Created by Jimmy Dee on 4/5/17.
-//  Copyright © 2017 Branch Metrics. All rights reserved.
-//
-
-#import "RCTSwiftLog.h"
